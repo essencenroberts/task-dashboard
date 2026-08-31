e55d2248169c2f94e6f1681ee49a8270c3d30f4e
Essence Roberts <essencenroberts@gmail.com>
2026-08-31 12:33:22 -0500

    inital commit, set up folder sturtures, delete starter code and files, install tailwindcss, react, and typescript with vite and icons

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='m-5 p-5 text-3xl font-bold'>Task Dashboard</h1>
+    </>
+  )
+}
+
+export default App
